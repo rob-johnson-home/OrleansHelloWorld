bed591daaeed6cfbfdf64dc6499ceea8514fab19
rob <rob.johnson.home@gmail.com>
2020-01-11 16:56:37 +0000

    initial commit

---
+++ GrainsInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrainsInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
